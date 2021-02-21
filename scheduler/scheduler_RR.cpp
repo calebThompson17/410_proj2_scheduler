9ef269773ad35e3147b5a95d33fbbbd12db007aa
caleb.thompson.17@cnu.edu <caleb.thompson.17@cnu.edu>
2021-02-21 00:03:40 -0500

    scheduler_RR compiling now

--- scheduler/scheduler_RR.cpp
+++ scheduler/scheduler_RR.cpp
@@ -11,20 +11,24 @@
 //TODO fill in content
 
 #include "../includes/scheduler_RR.h"
-#include "scheduler.h"
+#include "../includes/scheduler.h"
 
-class Scheduler_RR: public Scheduler {
-public:
+//override base class behaviour if necessary, otherwise call it
+bool  Scheduler_RR::time_to_switch_processes(int tick_count, PCB &p)
+{ return Scheduler::time_to_switch_processes(tick_count, p); }
 
-	//this is a preemptive scheduler,set the preemptive member variable to true
-	//when calling the base class
-	Scheduler_RR(std::queue<PCB> &queue,int time_slice =UNINITIALIZED):Scheduler(queue,true,time_slice){};
-	virtual ~Scheduler_RR(): ~Scheduler() {};
-
-	//override base class behaviour if necessary, otherwise call it
-	virtual bool  time_to_switch_processes(int tick_count, PCB &p)
-	{ return Scheduler::time_to_switch_processes(tick_count, p); }
-
-	//RR - preemptive - no sorting of ready_q needed.
-	virtual void sort();
-};
+//class Scheduler_RR: public Scheduler {
+//public:
+//
+//	//this is a preemptive scheduler,set the preemptive member variable to true
+//	//when calling the base class
+//	Scheduler_RR(std::queue<PCB> &queue,int time_slice =UNINITIALIZED):Scheduler(queue,true,time_slice){};
+//	virtual ~Scheduler_RR(): ~Scheduler() {};
+//
+//	//override base class behaviour if necessary, otherwise call it
+//	virtual bool  time_to_switch_processes(int tick_count, PCB &p)
+//	{ return Scheduler::time_to_switch_processes(tick_count, p); }
+//
+//	//RR - preemptive - no sorting of ready_q needed.
+//	virtual void sort();
+//};
