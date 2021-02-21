9040a716745622f5e542eac6a5c70c9d51e51b80
caleb.thompson.17@cnu.edu <caleb.thompson.17@cnu.edu>
2021-02-20 21:19:38 -0500

    Implemented scheduler_FIFO, I think...

--- scheduler/scheduler_FIFO.cpp
+++ scheduler/scheduler_FIFO.cpp
@@ -10,4 +10,20 @@
 
 //TODO fill in content
 
+#include "../includes/scheduler_FIFO.h"
+#include "scheduler.h"
 
+class Scheduler_FIFO: public Scheduler {
+public:
+	//this is a non - preemptive scheduler, set the preemptive member variable to false
+	//when calling the base class
+	Scheduler_FIFO(std::queue<PCB> &queue):Scheduler(queue,false,UNINITIALIZED){};
+	virtual ~Scheduler_FIFO(): ~Scheduler() {};
+
+	//override base class behaviour if necessary, otherwise call it
+	virtual bool  time_to_switch_processes(int tick_count, PCB &p)
+	{ return Scheduler::time_to_switch_processes(tick_count, p); }
+
+	//FIFO - not preemptive - no sorting needed
+	virtual void sort();
+};
