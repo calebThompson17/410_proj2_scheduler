cc9cbeaf8d23d87d54c2394eeef82b5f72bfb8c2
caleb.thompson.17@cnu.edu <caleb.thompson.17@cnu.edu>
2021-02-20 22:42:13 -0500

    Implemented scheduler_SRTF

--- scheduler/scheduler_SRTF.cpp
+++ scheduler/scheduler_SRTF.cpp
@@ -12,7 +12,10 @@
 
 #include "../includes/scheduler_SRTF.h"
 #include "scheduler.h"
+#include <algorithm>
 #include <vector>
+
+using namespace std;
 
 class Scheduler_SRTF: public Scheduler {
 public:
@@ -22,11 +25,34 @@ public:
 	virtual ~Scheduler_SRTF(): ~Scheduler() {};
 
 	//override base class behaviour if necessary, otherwise call it
-	virtual bool  time_to_switch_processes(int tick_count, PCB &p)
-	{ return Scheduler::time_to_switch_processes(tick_count, p); }
+	virtual bool  time_to_switch_processes(int tick_count, PCB &p) {
+		sort();
+		return Scheduler::time_to_switch_processes(tick_count, p);
+	}
+
+	// adding the sort method call
+	virtual void add(PCB p) {
+		Scheduler::add(p);
+		sort();
+	}
+
+	// Sort by remaining_CPU_Time
+	bool remainingCPUTimeSort(PCB p1, PCB p2) {
+		return p1.remaining_cpu_time < p2.remaining_cpu_time;
+	}
 
 	//SRTF-preemptive - sort ready_q by remaining_cpu_time whenever add(PCB p) or time_to_switch_processes is called
 	virtual void sort() {
 		vector<PCB> vec;
+		while (!ready_q->empty()) {
+			vec.push_back(ready_q->front());
+			ready_q->pop();
+		}
+		sort(vec.begin(), vec.end(), remainingCPUTimeSort);
+		while (!vec.empty()) {
+			ready_q->push(vec.front());
+			vec.erase(vec.begin());
+		}
+		delete vec;
 	}
 };
