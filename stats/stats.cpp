2fd05b0b6603407cd65da793dcdecdf0c72fd49b
caleb.thompson.17@cnu.edu <caleb.thompson.17@cnu.edu>
2021-02-20 23:44:28 -0500

    stats compiling now

--- stats/stats.cpp
+++ stats/stats.cpp
@@ -14,21 +14,24 @@
 
 using namespace std;
 
-Stats::Stats(std::vector<PCB> &finished_vector):
-				vec(finished_vector) { calcStats(); }
-
-virtual Stats::~Stats() { delete vec; }
+Stats::Stats(std::vector<PCB> &finished_vector) {
+	for (unsigned int i = 0; i < finished_vector.size(); i++) {
+		vec->push_back(finished_vector[i]);
+	}
+//	*vec = finished_vector.data();
+	calcStats();
+}
 
 //loops thru vec, prints 1 line for each process using the following format
 //Process 1 Required CPU time:2  arrived:0 started:0 finished:2
 //if there are 10 processes in vector, should print 10 lines
 void Stats::showAllProcessInfo() {
-	for (auto& it: vec) {
-		cout << "Process " << it.process_number <<
-				" Required CPU time: " << it.required_cpu_time <<
-				" arrived: " << it.arrival_time <<
-				" started: " << it.start_time <<
-				" finished: " << it.finish_time << endl;
+	for (unsigned int i=0; i < vec->size(); i++) {
+		cout << "Process " << (*vec)[i].process_number <<
+				" Required CPU time: " << (*vec)[i].required_cpu_time <<
+				" arrived: " << (*vec)[i].arrival_time <<
+				" started: " << (*vec)[i].start_time <<
+				" finished: " << (*vec)[i].finish_time << endl;
 	}
 }
 
@@ -55,10 +58,10 @@ void Stats::calcStats() {
 	av_wait_time = 0.0;
 	av_turnaround_time = 0.0;
 	av_response_time = 0.0;
-	for (auto& it: vec) {
-		av_wait_time += it.finish_time - it.arrival_time - it.required_cpu_time;
-		av_turnaround_time += it.finish_time - it.arrival_time;
-		av_response_time += it.start_time - it.arrival_time;
+	for (unsigned int i=0; i < vec->size(); i++) {
+		av_wait_time += (*vec)[i].finish_time - (*vec)[i].arrival_time - (*vec)[i].required_cpu_time;
+		av_turnaround_time += (*vec)[i].finish_time - (*vec)[i].arrival_time;
+		av_response_time += (*vec)[i].start_time - (*vec)[i].arrival_time;
 	}
 	av_wait_time /= vec->size();
 	av_turnaround_time /= vec->size();
